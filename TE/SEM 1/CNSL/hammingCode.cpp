cb2329fb511529aae16af9d067da4d4461731dc6
Vinayak Jamadar <thevinayaksjamadar2003@gmail.com>
2022-08-24 10:45:15 +0530

    Update hammingCode.cpp

--- TE/SEM 1/CNSL/hammingCode.cpp
+++ TE/SEM 1/CNSL/hammingCode.cpp
@@ -1,7 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void calculateRnValues(map<int, string>& rn, vector<int>& vec, int& len){
+	for (int i = 0; i < len; i++){
+		if(vec[i] == -1) cout<<"r"<<len-i<<"\t";
+		else cout<<vec[i]<<"\t";
+	}
+	cout<<"\n"<<endl;
+	map<int, string>::iterator itr;
+	for (itr = rn.begin(); itr != rn.end(); itr++)
+	{
+		int oneCount = 0;
+		itr->second = "";
+		for (int j = len-itr->first; j >= 0; j-=itr->first*2)
+		{
+			int k = 0;
+			while((j-k) >= 0 and k < itr->first)
+			{
+				if(vec[j-k] != -1){
+					itr->second += vec[j-k]+48;
+					if(vec[j-k] == 1) oneCount++;
+				}
+				k++;
+			}
+		}
+		cout<<"r"<<itr->first<<" = take "<<itr->first<<" and drop "<<itr->first<<" = "<<itr->second<<" = ";
+		if(oneCount%2) {
+			cout<<"odd = 1"<<endl;
+			itr->second = "1";
+			vec[len-itr->first] = 1;
+		}
+		else {
+			cout<<"even = 0"<<endl;
+			itr->second = "0";
+			vec[len-itr->first] = 0;
+		}
+	}
+}
+
 int main(){
+	// Reading Hamming Code
     string str;
     cout<<"Enter Hamming Code : ";
     cin>>str;
@@ -19,65 +57,34 @@ int main(){
     vector<int> vec(len, 0);
 
     // Assigning -1 for rn positions
-    for (auto &&i : rn)
+    map<int, string>::iterator i;
+    for (i = rn.begin(); i != rn.end(); i++)
     {
-        vec[len-i.first] = -1;
-    }
-    
-    int index = 0;
-    for (int i = 0; i < len; i++)
-    {
-        if(vec[i] == 0) vec[i] = (str[index]-48), index++;
+        vec[len-i->first] = -1;
     }
 
-    for (int itr = 0; itr < 2; itr++)
+    int index = 0;
+    for (int j = 0; j < len; j++)
     {
-        if (itr) cout<<"\nArray After Finding the Values of Rn's"<<endl;
-        else cout<<"\nArray Before Finding the Values of Rn's"<<endl;
-        for (int i = 0; i < len; i++)
-        {
-            if(vec[i] == -1) cout<<"r"<<len-i<<"\t";
-            else cout<<vec[i]<<"\t";
-        }
-        cout<<"\n"<<endl;
-        
-        for (auto &&i : rn)
-        {
-            int oneCount = 0;
-            for (int j = len-i.first; j >= 0; j-=i.first*2)
-            {
-                int k = 0;
-                while((j-k) >= 0 and k < i.first)
-                {
-                    if(vec[j-k] != -1){
-                        i.second += vec[j-k]+48;
-                        if(vec[j-k] == 1) oneCount++;
-                    }
-                    k++;
-                }
-            }
-            cout<<"r"<<i.first<<" = take "<<i.first<<" and drop "<<i.first<<" = "<<i.second<<" = ";
-            if(oneCount%2) {
-                cout<<"odd = 1"<<endl;
-                i.second = "1";
-                vec[len-i.first] = 1;
-            }
-            else {
-                cout<<"even = 0"<<endl;
-                i.second = "0";
-                vec[len-i.first] = 0;
-            }
-        }
+        if(vec[j] == 0) vec[j] = (str[index]-48), index++;
     }
-    
+
+    cout<<"\nArray Before Finding the Values of Rn's"<<endl;
+    calculateRnValues(rn, vec, len);
+    cout<<"\nArray After Finding the Values of Rn's"<<endl;
+    calculateRnValues(rn, vec, len);
+
+    // Checking whether all the rn values are 0 (i.e Even) or not
     bool flag = true;
-    for (auto &&i : rn)
+    for (i = rn.begin(); i != rn.end(); i++)
     {
-        if(i.second == "1") flag = false;
+        if(i->second == "1") flag = false;
     }
-    
+
+    // If all the rn values are 0 --> then Data is Transmitted
+    // Else --> then Data is Not Transmitted
     cout<<"\nResult : ";
-    if(flag) cout<<"Message Transmitted"<<endl;
-    else cout<<"Message Not Transmitted"<<endl;
+    if(flag) cout<<"Data is Transmitted"<<endl;
+    else cout<<"Data is Not Transmitted"<<endl;
     return 0;
 }
