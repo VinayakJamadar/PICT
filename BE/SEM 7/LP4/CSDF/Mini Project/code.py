9912ba18bd0fa150763b692ad3859d1af6feb7b0
Vinayak Jamadar <thevinayaksjamadar2003@gmail.com>
2023-11-06 22:35:24 +0530

    Added CSDF: Assignments, Mini Project and IR Mini Project

---
+++ BE/SEM 7/LP4/CSDF/Mini Project/code.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageChops
+
+def calculate_ela(original_image_path, manipulated_image_path, scale=10):
+    # Open the original and manipulated images
+    original = Image.open(original_image_path)
+    manipulated = Image.open(manipulated_image_path)
+
+    # Resize the manipulated image to match the original's dimensions
+    manipulated = manipulated.resize(original.size)
+
+    # Calculate the ELA image
+    ela_image = ImageChops.difference(original, manipulated)
+    ela_image = ela_image.convert('L')
+    ela_image = ela_image.point(lambda x: x * scale)
+
+    return ela_image
+
+def main():
+    original_image_path = 'original.jpg'
+    manipulated_image_path = 'manipulated.jpg'
+    
+    scale = 10  # Adjust the scale factor for the ELA image
+
+    ela_image = calculate_ela(original_image_path, manipulated_image_path, scale)
+    ela_image.show()
+    ela_image.save('ela_image1.jpg')
+
+if __name__ == '__main__':
+    main()
